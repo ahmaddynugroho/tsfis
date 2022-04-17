6eca3e5e0134392d3d77cd04cedcfd10a57bc88e
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-04-17 15:52:58 +0700

    feat: add variable & set

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { triangleFactory } from "./index";
+import { Domain, inferenceSystem, triangleFactory } from "./index";
 
 describe("triangle factory (2, 6, 10)", () => {
   const triangle = triangleFactory(2, 6, 10);
@@ -13,5 +13,106 @@ describe("triangle factory (2, 6, 10)", () => {
   });
   test("argument: 10, equal to 0", () => {
     expect(triangle(10)).toBe(0);
+  });
+});
+
+describe("check fuzzy sets", () => {
+  const area: Domain[] = [
+    {
+      name: "kecil",
+      border: [2, 6, 10],
+      shape: "triangular",
+    },
+  ];
+  test("domain membership function", () => {
+    const [a, b, c] = area[0].border;
+    expect(triangleFactory(a, b, c)(8)).toBe(0.5);
+  });
+});
+
+describe("inferenceSystem factory", () => {
+  test("add variable", () => {
+    const FS = inferenceSystem("sugeno");
+    FS.addVariable("luas", [
+      {
+        name: "kecil",
+        border: [0, 10, 20],
+        shape: "triangular",
+      },
+    ]);
+    const vars = FS.addVariable("keharuman", [
+      {
+        name: "bau",
+        border: [0, 5, 10],
+        shape: "triangular",
+      },
+    ]);
+
+    expect(vars[0].name).toBe("luas");
+    expect(vars[0].value).toBe(0);
+    expect(vars[0].set).toEqual([
+      {
+        name: "kecil",
+        border: [0, 10, 20],
+        shape: "triangular",
+      },
+    ]);
+    expect(vars[1].name).toBe("keharuman");
+    expect(vars[1].value).toBe(0);
+    expect(vars[1].set).toEqual([
+      {
+        name: "bau",
+        border: [0, 5, 10],
+        shape: "triangular",
+      },
+    ]);
+  });
+
+  test("add set", () => {
+    const FS = inferenceSystem("sugeno");
+    FS.addVariable("luas", [
+      {
+        name: "kecil",
+        border: [0, 10, 20],
+        shape: "triangular",
+      },
+    ]);
+
+    const setLuas = FS.addSet("luas", {
+      name: "sedang",
+      border: [15, 25, 36],
+      shape: "triangular",
+    });
+    expect(setLuas.name).toBe("luas");
+    expect(setLuas.set.pop()).toEqual({
+      name: "sedang",
+      border: [15, 25, 36],
+      shape: "triangular",
+    });
+  });
+  test("throw console.error on variable typo", () => {
+    const spyMock = jest
+      .spyOn(console, "error")
+      .mockImplementationOnce(() => {});
+    const FS = inferenceSystem("sugeno");
+    FS.addVariable("luas", [
+      {
+        name: "kecil",
+        border: [0, 10, 20],
+        shape: "triangular",
+      },
+    ]);
+    const err = FS.addSet("luass", {
+      name: "sedang",
+      border: [15, 25, 36],
+      shape: "triangular",
+    });
+
+    expect(spyMock).toHaveBeenCalled();
+    expect(err).not.toEqual({
+      name: "sedang",
+      border: [15, 25, 36],
+      shape: "triangular",
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,87 @@
 // 5. tsukamoto
 //
 // todo:
-// 1. function membership
+// 1. function membership (done)
 // 2. fuzzy sets
 // 3. variable
 // 4. tsukamoto inference
+//
+// component:
+// 1. input variables
+// 2. rules
+// 3. output variable
+//
+// flow:
+// 1. input variables
+// 2. rules
+// 3. input
+// 4. infer
 
-// Triangle membership function
+type MembershipFunction = (x: number) => number;
+type InferenceMethod = "sugeno" | "mamdani" | "tsukamoto";
+type TriangularBorder = [number, number, number];
+type TrapezoidalBorder = [number, number, number, number];
+
+export interface Domain {
+  name: string;
+  border: TriangularBorder | TrapezoidalBorder;
+  shape: "triangular" | "trapezoidal";
+}
+export interface Variable {
+  name: string;
+  value: number;
+  set: Domain[];
+}
+export interface InferenceSystemData {
+  method: InferenceMethod;
+  variable: Variable[];
+  rule: any[];
+}
+
 export function triangleFactory(
   a: number,
   b: number,
   c: number
-): (x: number) => number {
+): MembershipFunction {
   return (x: number): number => {
     if (x < a || x > c) return 0;
     if (x < b) return (x - a) / (b - a);
-    if (x < c) return (c - x) / (c - b);
+    if (x <= c) return (c - x) / (c - b);
   };
 }
 
-const triangle = triangleFactory(2, 6, 10);
-console.log(triangle(8));
-console.log(triangle(6));
-console.log(triangle(2));
-console.log(triangle(10));
+// debug is used in jest
+// TODO: add return type
+export function inferenceSystem(method: InferenceMethod) {
+  const data: InferenceSystemData = {
+    method,
+    variable: [],
+    rule: [],
+  };
+  const addVariable = (name: string, set: Domain[]): Variable[] => {
+    data.variable.push({
+      name,
+      value: 0,
+      set,
+    });
+    return data.variable;
+  };
+  const addSet = (variableName: string, domain: Domain): Variable => {
+    let modifiedVarIndex = 0;
+    let found = false;
+    data.variable.forEach((v, i) => {
+      if (v.name === variableName) {
+        v.set.push(domain);
+        modifiedVarIndex = i;
+        found = true;
+      }
+    });
+    if (!found) console.error("variable:", variableName, "not found");
+    return data.variable[modifiedVarIndex];
+  };
+
+  return {
+    addVariable,
+    addSet,
+  };
+}
